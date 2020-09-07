e1077b9008d25924eb186a0fce2e3bf136922c2d
landinialejandro <landinialejandro@hotmail.com>
2020-09-07 20:18:56 -0300

    invoice working

--- hooks/Invoice-dv.js
+++ hooks/Invoice-dv.js
@@ -23,5 +23,7 @@ $j(function() {
     inline_fields(['Phone', 'Email'], "Contact", [3, 6])
     inline_fields(['Address', 'City', 'Country'], "Address", [3, 3, 3])
     inline_fields(['AmountDUE', 'AmountPAID', 'Balance'], "Amount(due/paid/balance)", [3, 3, 3])
+    inline_fields(['usrAdd', 'whenAdd'], "Created By", [3, 3])
+    inline_fields(['usrUpdated', 'whenUpdated'], "Updated By", [3, 3])
 
 });
--- hooks/Invoice.php
+++ hooks/Invoice.php
@@ -1,113 +1,164 @@
 <?php
-	// For help on using hooks, please refer to https://bigprof.com/appgini/help/working-with-generated-web-database-application/hooks
+// For help on using hooks, please refer to https://bigprof.com/appgini/help/working-with-generated-web-database-application/hooks
 
-	function Invoice_init(&$options, $memberInfo, &$args) {
+function Invoice_init(&$options, $memberInfo, &$args)
+{
 
-		return TRUE;
+	return TRUE;
+}
+
+function Invoice_header($contentType, $memberInfo, &$args)
+{
+	$header = '';
+
+	switch ($contentType) {
+		case 'tableview':
+			$header = '';
+			break;
+
+		case 'detailview':
+			$header = '';
+			break;
+
+		case 'tableview+detailview':
+			$header = '';
+			break;
+
+		case 'print-tableview':
+			$header = '';
+			break;
+
+		case 'print-detailview':
+			$header = '';
+			break;
+
+		case 'filters':
+			$header = '';
+			break;
 	}
 
-	function Invoice_header($contentType, $memberInfo, &$args) {
-		$header='';
+	return $header;
+}
 
-		switch($contentType) {
-			case 'tableview':
-				$header='';
-				break;
+function Invoice_footer($contentType, $memberInfo, &$args)
+{
+	$footer = '';
 
-			case 'detailview':
-				$header='';
-				break;
+	switch ($contentType) {
+		case 'tableview':
+			$footer = '';
+			break;
 
-			case 'tableview+detailview':
-				$header='';
-				break;
+		case 'detailview':
+			$footer = '';
+			break;
 
-			case 'print-tableview':
-				$header='';
-				break;
+		case 'tableview+detailview':
+			$footer = '';
+			break;
 
-			case 'print-detailview':
-				$header='';
-				break;
+		case 'print-tableview':
+			$footer = '';
+			break;
 
-			case 'filters':
-				$header='';
-				break;
+		case 'print-detailview':
+			$footer = '';
+			break;
+
+		case 'filters':
+			$footer = '';
+			break;
+	}
+
+	return $footer;
+}
+
+function Invoice_before_insert(&$data, $memberInfo, &$args)
+{
+
+	return TRUE;
+}
+
+function Invoice_after_insert($data, $memberInfo, &$args)
+{
+
+	return TRUE;
+}
+
+function Invoice_before_update(&$data, $memberInfo, &$args)
+{
+	/* retrieve Invoice info */
+	///////////////////////////
+	$Invoice = getDataTable('Invoice', $data['selectedID']);
+
+	if ($Invoice['Status'] === 'CLOSED') {
+		$_SESSION['custom_msg'] = [
+			"message" => "<h4>La orden está cerrada, no se puede relizar cambios</h4>",
+			"class" => "danger",
+			"dismiss" => "0"
+		];
+		return FALSE;
+	}
+
+	if ($Invoice['type'] !== $data['type']){
+		$_SESSION['custom_msg'] = [
+			"message" => "<h4>No es posible cambiar el tipo de documento una vez generado.<br>
+							Por favor generar otro ducumento o relice una copia de este.</h4>",
+			"class" => "danger",
+			"dismiss" => "0"
+		];
+		return FALSE;
+	}
+
+	if ($data['Status'] === 'CLOSED' && $Invoice['Status'] === 'OPEN') {
+		// Cierra orden
+		// agregar movimiento a cashflow si no es QUOTE
+		$_SESSION['custom_msg'] = [
+			"message" => "<h4>La orden se está cerrando</h4>",
+			"class" => "success",
+			"dismiss" => "5"
+		];
+		if ($Invoice['type'] !== 'Quote'){
+			$accouting = [
+				"invoice" =>$data['selectedID'],
+				"date" => $data['Date'],
+				"description" => 'MOVIMIENTO por venta. Invoice:'. $data['number'],
+				
+			];
+			$insert = insert('Accounting',$accouting);
 		}
-
-		return $header;
 	}
 
-	function Invoice_footer($contentType, $memberInfo, &$args) {
-		$footer='';
+	return TRUE;
+}
 
-		switch($contentType) {
-			case 'tableview':
-				$footer='';
-				break;
+function Invoice_after_update($data, $memberInfo, &$args)
+{
 
-			case 'detailview':
-				$footer='';
-				break;
+	return TRUE;
+}
 
-			case 'tableview+detailview':
-				$footer='';
-				break;
+function Invoice_before_delete($selectedID, &$skipChecks, $memberInfo, &$args)
+{
 
-			case 'print-tableview':
-				$footer='';
-				break;
+	return TRUE;
+}
 
-			case 'print-detailview':
-				$footer='';
-				break;
+function Invoice_after_delete($selectedID, $memberInfo, &$args)
+{
+}
 
-			case 'filters':
-				$footer='';
-				break;
-		}
+function Invoice_dv($selectedID, $memberInfo, &$html, &$args)
+{
+}
 
-		return $footer;
-	}
+function Invoice_csv($query, $memberInfo, &$args)
+{
 
-	function Invoice_before_insert(&$data, $memberInfo, &$args) {
+	return $query;
+}
+function Invoice_batch_actions(&$args)
+{
 
-		return TRUE;
-	}
-
-	function Invoice_after_insert($data, $memberInfo, &$args) {
-
-		return TRUE;
-	}
-
-	function Invoice_before_update(&$data, $memberInfo, &$args) {
-
-		return TRUE;
-	}
-
-	function Invoice_after_update($data, $memberInfo, &$args) {
-
-		return TRUE;
-	}
-
-	function Invoice_before_delete($selectedID, &$skipChecks, $memberInfo, &$args) {
-
-		return TRUE;
-	}
-
-	function Invoice_after_delete($selectedID, $memberInfo, &$args) {
-
-	}
-
-	function Invoice_dv($selectedID, $memberInfo, &$html, &$args) {
-
-	}
-
-	function Invoice_csv($query, $memberInfo, &$args) {
-
-		return $query;
-	}
-	function Invoice_batch_actions(&$args) {
-
-		return array();
-	}
+	return array();
+}
