24473b5e3d4084935c59ca95a66a7eb511234521
landinialejandro <landinialejandro@hotmail.com>
2020-10-10 20:34:31 -0300

    fix product js

--- hooks/Products-tv.js
+++ hooks/Products-tv.js
@@ -1,3 +1,10 @@
 function thisTable() {
     return 'Products';
 }
+$j(function() {
+    removeEmpty();
+    showTumbs();
+    $j('.dl-horizontal').addClass('row')
+    $j('dt').addClass('col-3')
+    $j('dd').addClass('col-9')
+});
